6185e32b05b35e2232d191a9c0142caf1ed44ab7
Terry Patterson <10890854+TerryRPatterson@users.noreply.github.com>
2018-02-16 14:28:08 -0500

    Solved second test

--- python/algorithmFriyay/2-16/CreditCardVaildator.py
+++ python/algorithmFriyay/2-16/CreditCardVaildator.py
@@ -2,7 +2,17 @@
 
 
 def isValidNumber(number):
-    pass
+    stringedNumber = str(number)
+    if stringedNumber[0:2] == "37" or stringedNumber[0:2] == "34":
+        if not len(stringedNumber) == 15:
+            return "Card number not vaild"
+    if not (len(str(number)) == 16):
+        print("if 1 failed")
+        print(len(str(number)))
+        return "Card number not vaild"
+    if str(number)[0:4] == "6011":
+        return "Discover"
 
 
-assert isValid(6022) == "Discover", "The test failed"
+assert isValidNumber(6011000000000000) == "Discover", "The test failed"
+assert isValidNumber(3781908473920192) == "Card number not vaild", "The test failed 2"
