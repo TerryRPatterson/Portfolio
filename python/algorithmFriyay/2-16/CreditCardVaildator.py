8b41c4e7641904c8e89b15150411156a7e437273
Terry Patterson <10890854+TerryRPatterson@users.noreply.github.com>
2018-02-16 14:54:08 -0500

    Add master card check

--- python/algorithmFriyay/2-16/CreditCardVaildator.py
+++ python/algorithmFriyay/2-16/CreditCardVaildator.py
@@ -16,4 +16,4 @@ def isValidNumber(number):
 assert isValidNumber(6011000000000000) == "Discover", "The test failed excpeted output was Discover recived %d" %isValidNumber(6011000000000000)
 assert isValidNumber(3781908473920192) == "Card number not vaild", "The test failed 2"
 assert isValidNumber(4000000000000000) == "Visa", "The visa test failed"
-assert isValidNumber(5300000000000000) == "Master Card" && isValidNumber(5500000000000000) == "Master Card", "Text failed expected two master cards did not get"
+assert isValidNumber(5300000000000000) == "Master Card" and isValidNumber(5500000000000000) == "Master Card", "Text failed expected two master cards did not get"
