8b2602265889f12899858e01d12b0819c4ee6bda
Terry Patterson <10890854+TerryRPatterson@users.noreply.github.com>
2018-02-20 10:24:20 -0500

    Update Terry.py
    Adjusted length check for function

--- python/Terry.py
+++ python/Terry.py
@@ -1,12 +1,12 @@
 def isLetter(string):
-    if len(string) > 1:
+    if not len(string) == 1:
         raise TypeError("Get case excpeted a string of 1.")
     elif ord(string.lower()) >= 97 and ord(string.lower()) <= 122:
         return(True)
     else:
         return False
 def getCase(string):
-    if len(string) > 1:
+    if not len(string) == 1:
         raise TypeError("Get case excpeted a string of 1.")
     elif ord(string) >= 97 and ord(string) <= 122:
         return("lower")
